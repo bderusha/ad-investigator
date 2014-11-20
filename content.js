0a4a54268723080e1765b2f53499a91675500d9a
Matt Womer <mwomer@dataxu.com>
2014-11-20 16:17:45 -0500

    last checkin, sigh

--- content.js
+++ content.js
@@ -1,255 +1,276 @@
 /*jshint browser:true*/
 /*global chrome */
 'use strict';
-console.log('sending started message');
-chrome.extension.sendMessage('started2', function() {console.log('Received callback');});
-chrome.runtime.sendMessage('started', function() {console.log('Received callback');});
+
 setTimeout(go, 5000);
+
 function go() {
-  console.log("GOOOOO!!!!!");
-var patterns = [{
-  'name': 'DoubleClick (activity)',
-  'pattern': 'doubleclick\\.net\\/activity',
-  'type': 'activity',
-  'vendor': 'DoubleClick'
-}, {
-  'name': 'DoubleClick',
-  'pattern': 'ad\\.doubleclick\\.net\\/N',
-  'type': 'ad',
-  'vendor': 'DoubleClick'
-}, {
-  'name': 'DoubleClick (click tracker)',
-  'pattern': 'adclick\\.g\\.doubleclick\\.net\\/aclk',
-  'type': 'click',
-  'vendor': 'DoubleClick'
-}, {
-  'name': 'DoubleClick (impression tracker)',
-  'pattern': 'doubleclick\\.net\\/imp',
-  'type': 'impression',
-  'vendor': 'DoubleClick'
-}, {
-  'name': 'DoubleClick (ad)',
-  'pattern': 'doubleclick\\.net\\/ad.*?/',
-  'type': 'ad',
-  'vendor': 'DoubleClick'
-}, {
-  'name': 'DoubleClick (activity tracker)',
-  'pattern': 'doubleclick\\.net\\/activity',
-  'type': 'activity',
-  'vendor': 'DoubleClick'
-},
+  console.log('GOOOOO!!!!!');
+  var patterns = [{
+      'name': 'DoubleClick (activity)',
+      'pattern': 'doubleclick\\.net\\/activity',
+      'type': 'activity',
+      'vendor': 'DoubleClick'
+    }, {
+      'name': 'DoubleClick',
+      'pattern': 'ad\\.doubleclick\\.net\\/N',
+      'type': 'ad',
+      'vendor': 'DoubleClick'
+    }, {
+      'name': 'DoubleClick (click tracker)',
+      'pattern': 'adclick\\.g\\.doubleclick\\.net\\/aclk',
+      'type': 'click',
+      'vendor': 'DoubleClick'
+    }, {
+      'name': 'DoubleClick (impression tracker)',
+      'pattern': 'doubleclick\\.net\\/imp',
+      'type': 'impression',
+      'vendor': 'DoubleClick'
+    }, {
+      'name': 'DoubleClick (ad)',
+      'pattern': 'doubleclick\\.net\\/ad.*?/',
+      'type': 'ad',
+      'vendor': 'DoubleClick'
+    }, {
+      'name': 'DoubleClick (activity tracker)',
+      'pattern': 'doubleclick\\.net\\/activity',
+      'type': 'activity',
+      'vendor': 'DoubleClick'
+    },
 
-{
-  'name': 'DataXu ',
-  'pattern': 'rtb.*?dataxu\\.net\\/x',
-  'type': 'rtb',
-  'vendor': 'DataXu'
-},
-{
-  'name': 'DataXu (click tracker)',
-  'pattern': 'i\\.w55c\\.net\\/cl',
-  'type': 'click',
-  'vendor': 'DataXu'
-}, {
-  'name': 'DataXu',
-  'pattern': 'i\\.w55c\\.net\\/ping_match',
-  'type': 'cookiematch',
-  'vendor': 'DataXu'
-}, {
-  'name': 'DataXu (retargeting)',
-  'pattern': 'tags\\.w55c\\.net\\/rs',
-  'type': 'retarget',
-  'vendor': 'DataXu'
-},{
-  'name': 'DataXu (DX4GM)',
-  'pattern': 'tags\\.w55c\\.net\\/pi\\/trs998',
-  'type': 'privateinventory',
-  'vendor': 'DataXu'
-},
-{
-  'name': 'DataXu',
-  'pattern': 'i\\.w55c\\.net\\/a.gif',
-  'type': 'impression',
-  'vendor': 'DataXu'
-}, {
-  'name': 'DataXu (cookie matcher)',
-  'pattern': 'cti\\.w55c\\.net\\/cms-2c\\.html',
-  'type': 'cookiematch',
-  'vendor': 'DataXu'
-}, {
-  'name': 'Rubicon (impression cookie matcher)',
-  'pattern': 'cti\\.w55c\\.net\\/rubicon-cms-2.?\\.html',
-  'type': 'cookiematch',
-  'vendor': 'Rubicon'
-}, {
-  'name': 'DataXu Ad Server (Display)',
-  'pattern': 'ads\\.w55c\\.net/t/d',
-  'type': 'ad',
-  'vendor': 'DataXu'
-}, {
-  'name': 'DataXu Ad Server (Video)',
-  'pattern': 'ads\\.w55c\\.net\\/t\\/v',
-  'type': 'ad',
-  'vendor': 'DataXu'
-}, {
-  'name': 'DataXu Ad (insecure)',
-  'pattern': 'cdn\\.w55c\\.net\\/i\\/0R',
-  'type': 'ad',
-  'vendor': 'DataXu'
-}, {
-  'name': 'DataXu Ad (secure)',
-  'pattern': 'https:\\/\\/cdn\\.w55c\\.net\\/i\\/\\/s_0R',
-  'type': 'ad',
-  'vendor': 'DataXu'
-}, {
-  'name': 'DataXu Ad Server (Display)',
-  'pattern': 'ads\\.w55c\\.net/t/d',
-  'type': 'ad',
-  'vendor': 'DataXu'
-}, {
-  'name': 'PointRoll (impression tracker)',
-  'pattern': 't\\.pointroll\\.com',
-  'type': 'impression',
-  'vendor': 'PointRoll'
-}, {
-  'name': 'PointRoll (click tracker)',
-  'pattern': 'clk\\.pointroll\\.com',
-  'type': 'click',
-  'vendor': 'PointRoll'
-}, {
-  'name': 'PointRoll (ad server)',
-  'pattern': 'ad\\.pointroll\\.com',
-  'type': 'ad',
-  'vendor': 'PointRoll'
-}, {
-  'name': 'PointRoll (performance tracker)',
-  'pattern': '[speed|spd]\\.pointroll\\.com',
-  'type': 'tracker',
-  'vendor': 'PointRoll'
-}, {
-  'name': 'Demdex (activity tracker)',
-  'pattern': 'demdex\\.com\\/event',
-  'type': 'activity',
-  'vendor': 'DemDex'
-}, {
-  'name': 'Turn (activity tracker)',
-  'pattern': 'r\\.turn\\.com\\/event',
-  'type': 'activity',
-  'vendor': 'Turn'
-}, {
-  'name': 'AdapTV (retargeting)',
-  'pattern': 'segments\\.adaptv\\.com',
-  'type': 'retarget',
-  'vendor': 'AdapTV'
-}, {
-  'name': 'LiveRail',
-  'pattern': 'ad\\d\\.liverail\\.com',
-  'type': '',
-  'vendor': 'LiveRail'
-}, {
-  'name': 'OBA',
-  'pattern': 'surly\\.com',
-  'type': 'oba',
-  'vendor': 'Ghostery'
-}];
-
-//var href = 'http://i.w55c.net/cl?t=1&btid=NTQ2RTAwOEYwMDA2N0M2RDBBMEMwNTEzMTkwMDMzRjR8R0ZuN1JheWtGRnwxNDE2NDk1MjQ3NTM4fDF8MEZwV0FCdE1kTnwwUjRZRXF4VmVnfDZjMDk5MzhlLTI1MmUtNDZmNC04Mjk2LTY0YjI0ODZiZTUwNXw3MDAwMDAwfHx8fDEyLjBQfFVTRA&ei=GOOGLE&tpc=http://adclick.g.doubleclick.net/aclk%3Fsa%3Dl%26ai%3DCBpDYjwBuVO34GZOKMPTngMgBzdzg3AW14_T3uAHAjbcBEAEgAGDJhu2IhKTsD4IBF2NhLXB1Yi0yOTU4OTI4NDg1NTk4OTYxoAGxtNXsA8gBCeACAKgDAaoEjQFP0AdWaMorOI0etWO-Hvnv_W18O-x1aKo8ukGvqQoGvHjmk6ihfrmAFhdordUvMiNNyWkpVk0dO3cfw45JLiz1tVyoWx0enL_rtPEU08VSUAGXM9p3KzGYKU4efksm4tJ1V1edXPOdHV-z-GWXVe_b-uL29SwhaotoftL6KEciTCahDOYqRJwgjI2s4_bgBAGABonzw8fe_MGs9gGgBiE%26num%3D1%26sig%3DAOD64_3Y_j4wUD6ZCaQ0nJAJ9e32hQskaA%26client%3Dca-pub-2958928485598961%26adurl%3D&rurl=https://dataxu.atlassian.net/wiki/pages/viewpage.action?spaceKey=Innovation&title=Innovation+Day+9+-+November+20th%252C+2014';
-var matcher = function(m, i) {
-  if (m && m.length > (i - 1)) {
-    return m[i];
-  }
-};
-var btid, exchange;
-var parseHref = function(href) {
-  btid = matcher(href.match(/btid=(.*?)&/), 1);
-  var tcp = matcher(href.match(/tcp=(.*?)&/), 1);
-  exchange = matcher(href.match(/ei=(.*?)&/), 1);
-
-  parseBtid(btid);
-};
-
-var parseBtid = function(btid) {
-  console.log('Parsing BTID: ' + btid);
-  if (!btid) return;
-  for (ii = 0; ii < 4; ii++) {
-    try {
-      var q = atob(btid);
-      var parsedBtid = q.split('|');
-      flight_uid = parsedBtid[4];
-      creative_uid = parsedBtid[5];
-      return;
-    } catch (e) {
-      btid += '=';
+    {
+      'name': 'DataXu ',
+      'pattern': 'rtb.*?dataxu\\.net\\/x',
+      'type': 'rtb',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu (click tracker)',
+      'pattern': 'i\\.w55c\\.net\\/cl',
+      'type': 'click',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu',
+      'pattern': 'i\\.w55c\\.net\\/ping_match',
+      'type': 'cookiematch',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu (retargeting)',
+      'pattern': 'tags\\.w55c\\.net\\/rs',
+      'type': 'retarget',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu (DX4GM)',
+      'pattern': 'tags\\.w55c\\.net\\/pi\\/trs998',
+      'type': 'privateinventory',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu',
+      'pattern': 'i\\.w55c\\.net\\/a.gif',
+      'type': 'impression',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu (cookie matcher)',
+      'pattern': 'cti\\.w55c\\.net\\/cms-2c\\.html',
+      'type': 'cookiematch',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'Rubicon (impression cookie matcher)',
+      'pattern': 'cti\\.w55c\\.net\\/rubicon-cms-2.?\\.html',
+      'type': 'cookiematch',
+      'vendor': 'Rubicon'
+    }, {
+      'name': 'DataXu Ad Server (Display)',
+      'pattern': 'ads\\.w55c\\.net/t/d',
+      'type': 'ad',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu Ad Server (Video)',
+      'pattern': 'ads\\.w55c\\.net\\/t\\/v',
+      'type': 'ad',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu Ad (insecure)',
+      'pattern': 'cdn\\.w55c\\.net\\/i\\/0R',
+      'type': 'ad',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu Ad (secure)',
+      'pattern': 'https:\\/\\/cdn\\.w55c\\.net\\/i\\/\\/s_0R',
+      'type': 'ad',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'DataXu Ad Server (Display)',
+      'pattern': 'ads\\.w55c\\.net/t/d',
+      'type': 'ad',
+      'vendor': 'DataXu'
+    }, {
+      'name': 'PointRoll (impression tracker)',
+      'pattern': 't\\.pointroll\\.com',
+      'type': 'impression',
+      'vendor': 'PointRoll'
+    }, {
+      'name': 'PointRoll (click tracker)',
+      'pattern': 'clk\\.pointroll\\.com',
+      'type': 'click',
+      'vendor': 'PointRoll'
+    }, {
+      'name': 'PointRoll (ad server)',
+      'pattern': 'ad\\.pointroll\\.com',
+      'type': 'ad',
+      'vendor': 'PointRoll'
+    }, {
+      'name': 'PointRoll (performance tracker)',
+      'pattern': '[speed|spd]\\.pointroll\\.com',
+      'type': 'tracker',
+      'vendor': 'PointRoll'
+    }, {
+      'name': 'Demdex (activity tracker)',
+      'pattern': 'demdex\\.com\\/event',
+      'type': 'activity',
+      'vendor': 'DemDex'
+    }, {
+      'name': 'Turn (activity tracker)',
+      'pattern': 'r\\.turn\\.com\\/event',
+      'type': 'activity',
+      'vendor': 'Turn'
+    }, {
+      'name': 'AdapTV (retargeting)',
+      'pattern': 'segments\\.adaptv\\.com',
+      'type': 'retarget',
+      'vendor': 'AdapTV'
+    }, {
+      'name': 'LiveRail',
+      'pattern': 'ad\\d\\.liverail\\.com',
+      'type': '',
+      'vendor': 'LiveRail'
+    }, {
+      'name': 'OBA',
+      'pattern': 'surly\\.com',
+      'type': 'oba',
+      'vendor': 'Ghostery'
     }
-  }
-  console.log('Could not decode BTID: ' + btid);
-  return;
-};
+  ];
 
-var matchPattern = function(uri) {
-  var res = [];
-  var numFound = 0;
-  for (var ii = 0; ii < patterns.length; ii++) {
-    var pat = patterns[ii];
-    //    console.log('Trying pattern: ' + pat.pattern);
-    if (uri.match(pat.pattern)) {
-      numFound++;
-      console.log('Matched pattern "' + pat.name + '" type: ' + pat.type);
-      return pat;
+  //var href = 'http://i.w55c.net/cl?t=1&btid=NTQ2RTAwOEYwMDA2N0M2RDBBMEMwNTEzMTkwMDMzRjR8R0ZuN1JheWtGRnwxNDE2NDk1MjQ3NTM4fDF8MEZwV0FCdE1kTnwwUjRZRXF4VmVnfDZjMDk5MzhlLTI1MmUtNDZmNC04Mjk2LTY0YjI0ODZiZTUwNXw3MDAwMDAwfHx8fDEyLjBQfFVTRA&ei=GOOGLE&tpc=http://adclick.g.doubleclick.net/aclk%3Fsa%3Dl%26ai%3DCBpDYjwBuVO34GZOKMPTngMgBzdzg3AW14_T3uAHAjbcBEAEgAGDJhu2IhKTsD4IBF2NhLXB1Yi0yOTU4OTI4NDg1NTk4OTYxoAGxtNXsA8gBCeACAKgDAaoEjQFP0AdWaMorOI0etWO-Hvnv_W18O-x1aKo8ukGvqQoGvHjmk6ihfrmAFhdordUvMiNNyWkpVk0dO3cfw45JLiz1tVyoWx0enL_rtPEU08VSUAGXM9p3KzGYKU4efksm4tJ1V1edXPOdHV-z-GWXVe_b-uL29SwhaotoftL6KEciTCahDOYqRJwgjI2s4_bgBAGABonzw8fe_MGs9gGgBiE%26num%3D1%26sig%3DAOD64_3Y_j4wUD6ZCaQ0nJAJ9e32hQskaA%26client%3Dca-pub-2958928485598961%26adurl%3D&rurl=https://dataxu.atlassian.net/wiki/pages/viewpage.action?spaceKey=Innovation&title=Innovation+Day+9+-+November+20th%252C+2014';
+  var matcher = function(m, i) {
+    if (m && m.length > (i - 1)) {
+      return m[i];
     }
-    // TODO: don't stop on first match
-  }
-  console.log('Could not find matching pattern for:  "' + uri + '"');
-  return;
-};
+  };
+  var btid, exchange;
+  var parseHref = function(href) {
+    btid = matcher(href.match(/btid=(.*?)&/), 1);
+    var tcp = matcher(href.match(/tcp=(.*?)&/), 1);
+    exchange = matcher(href.match(/ei=(.*?)&/), 1);
 
-var creative_uid, flight_uid, click_trackers, pixel_fires, cookie_matchers, vendors, macros, cdn_url, landing_page_url, href;
-var iframes = document.querySelectorAll('iframe');
-var links = document.querySelectorAll('a');
-var imgs = document.querySelectorAll('img');
-console.log('Found ' + imgs.length + ' images ' + links.length + ' links ' + iframes.length +' iframes');
-var results = [];
-var found = false;
+    parseBtid(btid);
+  };
 
-for (var ii = iframes.length; ii--;) {
-  var el = iframes[ii],
-    uri = el.getAttribute('src');
-  if (uri) {
-    console.log('Found iframe link: ' + uri);
-    var res = matchPattern(uri);
-    if (res) {
-      res.uri = uri;
-      console.dir(res);
-      results.push(res);
-      found = true;
-    }
-  }
-}
-for (var ii = imgs.length; ii--;) {
-  var el = imgs[ii],
-    uri = el.getAttribute('src');
-  if (uri) {
-    console.log('Found image ' + ii + ' link: ' + uri);
-    var res = matchPattern(uri);
-    if (res) {
-      res.uri = uri;
-      if (res.type==='rtb') {
-        parseHref(uri);
+  var parseBtid = function(btid) {
+    console.log('Parsing BTID: ' + btid);
+    if (!btid) return;
+    for (var kk = 0; kk < 4; kk++) {
+      try {
+        var q = atob(btid);
+        var parsedBtid = q.split('|');
+        flight_uid = parsedBtid[4];
+        creative_uid = parsedBtid[5];
+        return;
+      } catch (e) {
+        btid += '=';
       }
-      console.dir(res);
-      results.push(res);
-      found = true;
+    }
+    console.log('Could not decode BTID: ' + btid);
+    return;
+  };
+
+  var matchPattern = function(uri) {
+    var res = [];
+    var numFound = 0;
+    for (var jj = 0; jj < patterns.length; jj++) {
+      var pat = patterns[jj];
+      //    console.log('Trying pattern: ' + pat.pattern);
+      if (uri.match(pat.pattern)) {
+        numFound++;
+        console.log('Matched pattern "' + pat.name + '" type: ' + pat.type);
+        return pat;
+      }
+      // TODO: don't stop on first match
+    }
+    console.log('Could not find matching pattern for:  "' + uri + '"');
+    return;
+  };
+
+  var creative_uid, flight_uid, click_trackers, pixel_fires, cookie_matchers, vendors, macros, cdn_url, landing_page_url, href;
+  var iframes = document.querySelectorAll('iframe');
+  var links = document.querySelectorAll('a');
+  var imgs = document.querySelectorAll('img');
+  var embeds = document.querySelectorAll('object');
+
+  console.log('Found ' + imgs.length + ' images ' + links.length + ' links ' + iframes.length + ' iframes ' + embeds.length + ' embeds');
+  var results = [];
+  var found = false;
+  var el, res, uri, flashVars;
+
+  for (var ii = iframes.length; ii--;) {
+    el = iframes[ii],
+    uri = el.getAttribute('src');
+    if (uri) {
+      console.log('Found iframe link: ' + uri);
+      res = matchPattern(uri);
+      if (res) {
+        res.uri = uri;
+        console.dir(res);
+        results.push(res);
+        found = true;
+      }
     }
   }
-}
+  for (ii = 0; ii < imgs.length; ii++) {
+    console.log('image ' + ii);
+    el = imgs[ii];
+    uri = el.getAttribute('src');
+    if (uri) {
+      console.log('Found image ' + ii + ' link: ' + uri);
+      res = matchPattern(uri);
+      if (res) {
+        res.uri = uri;
+        if (res.type === 'rtb') {
+          parseHref(uri);
+        }
+        console.dir(res);
+        results.push(res);
+        found = true;
+      }
+    }
+  }
 
 
-for (ii = links.length; ii--;) {
-  var el = links[ii],
+  for (ii = links.length; ii--;) {
+    el = links[ii],
     uri = el.getAttribute('href');
-  if (uri) {
-    console.log('Found anchor link: ' + uri);
-    var res = matchPattern(uri);
+    if (uri) {
+      console.log('Found anchor link: ' + uri);
+      res = matchPattern(uri);
+      if (res) {
+        res.uri = uri;
+        console.dir(res);
+        results.push(res);
+        found = true;
+      }
+    }
+  }
+
+  for (ii = embeds.length; ii--;) {
+    el = embeds[ii];
+    flashVars = el.getAttribute('flashvars');
+    console.log('Got flashVars: ' + flashVars);
+    var matches = flashVars.match('clickTag=(.*?)&');
+    
+    if (matches.length > 0) {
+      uri = decodeURIComponent(matches[1]);      
+    console.log('Flash click uri: ' + uri);
+    res = matchPattern(uri);
     if (res) {
       res.uri = uri;
       console.dir(res);
@@ -257,63 +278,80 @@ for (ii = links.length; ii--;) {
       found = true;
     }
   }
-}
-var msg = {};
-msg.click_trackers = [];
-msg.impression_trackers = [];
-msg.activity_trackers = [];
-var filled = false;
-for (var ii = 0; ii < results.length; ii++) {
-  var info = results[ii];
-  console.log('Info:');
-  console.dir(info);
-  if (info.type == 'ad') {
-    msg.type = 'ad';
-    filled = true;
-    msg.creative_uid = creative_uid;
-    msg.flight_uid = flight_uid;
-    msg.exchange = exchange;
+  }
 
-    if (info.vendor == 'DataXu') {
-      console.log('Found DataXu Ad');
-      parseHref(info.uri);
+
+  var msg = {};
+  msg.click_trackers = [];
+  msg.impression_trackers = [];
+  msg.activity_trackers = [];
+  msg.cookie_matchers = [];
+
+  var filled = false;
+  for (ii = 0; ii < results.length; ii++) {
+    var info = results[ii];
+    console.log('Info:');
+    console.dir(info);
+    if (info.type == 'ad') {
+      msg.type = 'ad';
+      filled = true;
       msg.creative_uid = creative_uid;
       msg.flight_uid = flight_uid;
       msg.exchange = exchange;
-    } else {
-      msg.ad_server = info.vendor;
+
+      if (info.vendor == 'DataXu') {
+        console.log('Found DataXu Ad');
+        parseHref(info.uri);
+        msg.creative_uid = creative_uid;
+        msg.flight_uid = flight_uid;
+        msg.exchange = exchange;
+      } else {
+        msg.ad_server = info.vendor;
+      }
+    } else if (info.type == 'impression') {
+      var imp = {};
+      imp.name = info.vendor;
+      imp.url = info.uri;
+      imp.is_secure = (info.uri.indexOf('https') === 0);
+      msg.impression_trackers.push(imp);
+      filled = true;
+
+    } else if (info.type == 'activity') {
+      var act = {};
+      act.name = info.vendor;
+      act.url = info.uri;
+      act.is_secure = (info.uri.indexOf('https') === 0);
+      msg.activity_trackers.push(act);
+      filled = true;
+
+    } else if (info.type == 'click') {
+      var click = {};
+      click.name = info.vendor;
+      if (info.uri) {
+        click.url = info.uri;
+        click.is_secure = (info.uri.indexOf('https') === 0);
+      }
+      msg.click_trackers.push(click);
+      filled = true;
+    } else if (info.type === 'cookiematch') {
+      var cm = {};
+      cm.name = info.vendor;
+      cm.url = info.uri;
+      cm.is_secure = (info.uri.indexOf('https') === 0);
+      msg.cookie_matchers.push(cm);
+      filled = true;
+
+
     }
-  } else if (info.type == 'impression') {
-    var imp = {};
-    imp.name = info.vendor;
-    imp.url = info.uri;
-    imp.is_secure = (info.uri.indexOf('https') === 0);
-    msg.impression_trackers.push(imp);
-    filled = true;
-
-  } else if (info.type == 'activity') {
-    var act = {};
-    act.name = info.vendor;
-    act.url = info.uri;
-    act.is_secure = (info.uri.indexOf('https') === 0);
-    msg.activity_trackers.push(act);
-    filled = true;
-
-  } else if (info.type == 'click') {
-    var click = {};
-    click.name = info.vendor;
-    click.url = info.uri;
-    click.is_secure = (info.uri.indexOf('https') === 0);
-    msg.click_trackers.push(click);
-    filled = true;
   }
-}
-if (filled) {
-  console.log('Sending message:');
-  console.dir(msg);
+  if (filled) {
+    console.log('Sending message:');
+    console.dir(msg);
 
-  chrome.extension.sendMessage(msg, function() {console.log('Received callback');});
-} else {
-  console.log('no ads found');
-}
+    chrome.extension.sendMessage(msg, function() {
+      console.log('Received callback');
+    });
+  } else {
+    console.log('no ads found');
+  }
 }
