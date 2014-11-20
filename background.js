011cf74d162d153643413d5d79a87f496e2b9203
Bill DeRusha <bderusha@dataxu.com>
2014-11-20 13:13:27 -0500

    build popup

--- background.js
+++ background.js
@@ -13,11 +13,53 @@ function getCreatives(callback){
   callback(creatives);
 }
 
-var creatives = [];
+var creatives = [
+  {
+    type: 'ad',
+    creative_uid: 'C12345',
+    flight_uid: 'F12345',
+    exchange: "GOOGLE",
+    ad_server: "DataXu",
+    click_trackers: [
+      {
+        name: 'Dataxu', //DFA, Pointroll, etc
+        info: "random string of stuff",
+        url: "http://i.w55c.net/cl",
+        is_secure: false
+      }
+    ],
+    pixel_fires:[
+      {
+        name: 'Dataxu', //DFA, Pointroll, etc
+        info: "random string of stuff",
+        url: "https://i.w55c.net/a.gif",
+        is_secure: true
+      }
+    ],
+    cookie_matchers:[
+      {
+        name: 'Dataxu', //DFA, Pointroll, etc
+        info: "random string of stuff",
+        is_secure: true
+      }
+    ],
+    vendors: [
+      "DataXu",
+      "DoubleClick"
+    ],
+    macros: [
+      "CACHEBUSTER",
+      "TIMESTAMP"
+    ],
+    cdn_url: "http://cdn.w55c.net/123/123/123",
+    is_secure: true,
+    landing_page_url: 'http://landing_page_url.com'
+  }
+];
 
 chrome.runtime.onMessage.addListener(function(message)  {
   if(message.type == 'ad'){
-    creatives.push(message);
+    //creatives.push(message);
   }
 });
 
--- popup.js
+++ popup.js
@@ -1,18 +1,16 @@
 function buildDOM(creatives)  {
   console.log(creatives);
   for(var i=0; i < creatives.length; i++){
-    console.log(i);
     var root = document.getElementById('template').cloneNode(true);
-    console.log(i);
     root.id = 'container_'+i;
-    console.log(i);
-    root.getElementsByClassName('flight')[0].appendChild(flightLink(creatives[i].flight_uid));
-    console.log(root);
+    root.setAttribute('data-secure', creatives[i].is_secure);
+    addFlightLink(creatives[i].flight_uid, root);
+    addCreativeLink(creatives[i].creative_uid, root);
     document.getElementsByTagName('body')[0].appendChild(root);
   }
 }
 
-function flightLink(fuid){
+function addFlightLink(fuid, root){
   var a = document.createElement('a');
   if( fuid ){
     var url = 'https://advertisers.dataxu.com/flights/'+fuid;
@@ -20,8 +18,100 @@ function flightLink(fuid){
     a.setAttribute('target', '_blank');
     a.innerHTML = url;
   }
-  return a;
+  root.getElementsByClassName('flight')[0].appendChild(a);
 }
+
+function addCreativeLink(cuid, root){
+  var a = document.createElement('a');
+  if( cuid ){
+    var url = 'https://advertisers.dataxu.com/search?utf8=✓&q='+cuid+'#tab_Creative';
+    a.setAttribute('href', url);
+    a.setAttribute('target', '_blank');
+    a.innerHTML = url;
+  }
+  root.getElementsByClassName('creative')[0].appendChild(a);
+}
+
+function addClickTrackers(trackers, root){
+  var ul = root.getElementsByClassName('click-trackers')[0].getElementsByTagName('ul')[0];
+  for(var i=0; i < trackers.length; i++){
+    var li = document.createElement('li');
+    li.innerHTML = trackers[i].name;
+    li.setAttribute('title', trackers[i].url);
+    if( root.getAttribute('data-secure') == 'true' && !trackers[i].is_secure ){
+      li.className = 'warning';
+    }
+    ul.appendChild(li);
+  }
+}
+
+function addImpressionTrackers(trackers, root){
+  var ul = root.getElementsByClassName('impression-trackers')[0].getElementsByTagName('ul')[0];
+  for(var i=0; i < trackers.length; i++){
+    var li = document.createElement('li');
+    li.innerHTML = trackers[i].name;
+    li.setAttribute('title', trackers[i].url);
+    if( root.getAttribute('data-secure') == 'true' && !trackers[i].is_secure ){
+      li.className = 'warning';
+    }
+    ul.appendChild(li);
+  }
+}
+
+function addCookieMatcher(matchers, root){
+  var ul = root.getElementsByClassName('cookie-matchers')[0].getElementsByTagName('ul')[0];
+  for(var i=0; i < matchers.length; i++){
+    var li = document.createElement('li');
+    li.innerHTML = matchers[i].name;
+    if( root.getAttribute('data-secure') == 'true' && !matchers[i].is_secure ){
+      li.className = 'warning';
+    }
+    ul.appendChild(li);
+  }
+}
+
+
+function addVendors(vendors, root){
+  var ul = root.getElementsByClassName('vendors')[0].getElementsByTagName('ul')[0];
+  for(var i=0; i < vendors.length; i++){
+    var li = document.createElement('li');
+    li.innerHTML = vendors[i];
+    ul.appendChild(li);
+  }
+}
+
+function addMacros(macros, root){
+  var ul = root.getElementsByClassName('macros')[0].getElementsByTagName('ul')[0];
+  for(var i=0; i < macros.length; i++){
+    var li = document.createElement('li');
+    li.innerHTML = macros[i];
+    ul.appendChild(li);
+  }
+}
+
+function addCdn(cdn, root){
+  var a = document.createElement('a');
+  if( cdn ){
+    var url = cdn;
+    a.setAttribute('href', url);
+    a.setAttribute('target', '_blank');
+    a.innerHTML = url;
+  }
+  root.getElementsByClassName('cdn-link')[0].appendChild(a);
+}
+
+function addLandingPage(page, root){
+  var a = document.createElement('a');
+  if( page ){
+    var url = page;
+    a.setAttribute('href', url);
+    a.setAttribute('target', '_blank');
+    a.innerHTML = url;
+  }
+  root.getElementsByClassName('landing-page')[0].appendChild(a);
+}
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
